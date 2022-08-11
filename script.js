7766668b9e6a4ecb624739d8085a9dc184358473
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-08-10 22:13:29 -0600

    added initial coding, trying out previous examples

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+var timerEl = document.getElementById('timer');
+var mainEl = document.getElementById('main');
 
+var message =
+  'Example Script';
+
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 75;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = 'Time: ' + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+      // Call the `displayMessage()` function
+      displayMessage();
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+// Displays the message one word at a time
+function displayMessage() {
+
+  // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var msgInterval = setInterval(function () {
+    // If there are no more questions left
+    if (words[wordCount] === undefined) {
+      // Use `clearInterval()` to stop the timer
+      clearInterval(msgInterval);
+    } else {
+      // Display message
+      mainEl.textContent = message;
+    }
+  }, 1000);
+}
+
+countdown();
