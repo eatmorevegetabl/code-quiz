d20b3fd8b5381686bc98ad0e6cc144d81dae18a0
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-08-11 23:58:37 -0600

    added score box

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,11 @@ function selectAnswer(event) {
   var El = event.target.getAttribute('id')
   console.log(El)
   var currentQuestion = questions[questionIndex];
-  if ()
+  if (El === 'button1') {
+    console.log('it works')
+  } else {
+    //timer reduces by 15 secs
+  }
 }
 
 function displayQuestion() {
@@ -43,8 +47,8 @@ var questions = [
   {
     question: 'question placeholder 2',
     answers: [
-      {text: 'answer placeholder 2.a', correct: true},
-      {text: 'answer placeholder 2.b', correct: false},
+      {text: 'answer placeholder 2.a', correct: false},
+      {text: 'answer placeholder 2.b', correct: true},
       {text: 'answer placeholder 2.c', correct: false},
       {text: 'answer placeholder 2.d', correct: false},
       ]
@@ -52,28 +56,28 @@ var questions = [
   {
     question: 'question placeholder 3',
     answers: [
-      {text: 'answer placeholder 3.a', correct: true},
+      {text: 'answer placeholder 3.a', correct: false},
       {text: 'answer placeholder 3.b', correct: false},
       {text: 'answer placeholder 3.c', correct: false},
-      {text: 'answer placeholder 3.d', correct: false},
+      {text: 'answer placeholder 3.d', correct: true},
       ]
   },
   {
     question: 'question placeholder 4',
     answers: [
-      {text: 'answer placeholder 4.a', correct: true},
+      {text: 'answer placeholder 4.a', correct: false},
       {text: 'answer placeholder 4.b', correct: false},
-      {text: 'answer placeholder 4.c', correct: false},
+      {text: 'answer placeholder 4.c', correct: true},
       {text: 'answer placeholder 4.d', correct: false},
       ]
   },
   {
     question: 'question placeholder 5',
     answers: [
-      {text: 'answer placeholder 5.a', correct: true},
+      {text: 'answer placeholder 5.a', correct: false},
       {text: 'answer placeholder 5.b', correct: false},
       {text: 'answer placeholder 5.c', correct: false},
-      {text: 'answer placeholder 5.d', correct: false},
+      {text: 'answer placeholder 5.d', correct: true},
       ]
   }
 ]
